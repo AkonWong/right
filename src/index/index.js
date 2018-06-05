c453f0bb91d63b092d75622edaba128bfdd612be
AkonHuang <hxw0717@gmail.com>
2018-06-05 16:13:55 +0800

    olala

--- src/index/index.js
+++ src/index/index.js
@@ -2,7 +2,7 @@
  * @Author: Akon Wong 
  * @Date: 2018-06-05 15:41:03 
  * @Last Modified by: Akon Wong
- * @Last Modified time: 2018-06-05 15:41:28
+ * @Last Modified time: 2018-06-05 16:13:31
  */
 
- let i, k, j;
+ let i, j;
